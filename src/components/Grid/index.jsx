add6c2e8d3788081a941f960e55995b6334e7680
iatraljic <iatraljic@gmail.com>
2020-05-15 16:00:40 +0200

    Created home page and grid component for responsive grid layout

---
+++ src/components/Grid/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+function Grid() {
+  return (
+    <div className="grid-container">
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+        <div className='col-1 t-col-2 m-col-4'>
+        </div>
+    </div>
+  );
+};
+
+export default Grid;
